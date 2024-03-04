d331258440f561b524c3646c191cca2d5b97d607
ashu.anuj <ashu.anuj@gmail.com>
2024-03-04 10:54:33 +0530

    CCPV-753 changes are completed

--- Hello1.java
+++ Hello1.java
@@ -3,4 +3,10 @@ public class Hello1{
 public static void main(String... args){
 System.out.println("Hello git");
 }
+
+//bug fix CCPV-753
+public void doProcess(){
+	System.out.println("hello DoProcess.. Need to write logic below");
+	//logic done
+}
 }
